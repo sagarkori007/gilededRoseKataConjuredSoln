0152279817cf6ca59ce784946ce35113c397373e
Sagar Kori <sagukori2022@gmail.com>
2024-08-03 17:18:42 +0530

    Simplify item update logic based on item name

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,36 +7,28 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
             if item.name == "Aged Brie":
                 if item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 1 and item.quality < 50:
                     item.quality += 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality += 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                pass
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 1:
+                    item.quality = 0
             else:
                 if item.quality > 0:
                     item.quality -= 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality += 1
-                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0
-                elif item.name == "Sulfuras, Hand of Ragnaros":
-                    pass
-                else:
-                    if item.quality > 0:
-                        item.quality -= 1
+                if item.sell_in < 1 and item.quality > 0:
+                    item.quality -= 1
+            item.sell_in -= 1
 
 
 class Item:
