aef5d136b0cddd9790cb07cd4c7307c45a8f49da
Sagar Kori <sagukori2022@gmail.com>
2024-08-03 17:04:49 +0530

    Refactor item update logic for clarity

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,22 +7,25 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Aged Brie":
                 if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                    item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality += 1
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality += 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality += 1
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                pass
+            else:
+                if item.quality > 0:
+                    item.quality -= 1
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
             if item.sell_in < 0:
                 if item.name == "Aged Brie":
                     if item.quality < 50:
